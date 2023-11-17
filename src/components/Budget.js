34f5ef6a4ffad59fefea23725e72e63e68305b7c
Patrick ODonnell <patrickjodonnell@comcast.net>
2023-11-17 08:30:19 -0500

    Added features to budget and allocation

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,9 +2,22 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
+        if (event.target.value > 20000) {
+            alert("Your budget cannot exceed $20,000");
+            setNewBudget(2000);
+            return;
+        }
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        if (event.target.value < totalExpenses) {
+            alert("Your budget cannot be less than you current total spending amount");
+            setNewBudget(2000);
+            return;
+        }
         setNewBudget(event.target.value);
     }
     return (
